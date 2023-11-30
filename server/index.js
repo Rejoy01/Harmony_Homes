11d73386492391afa9127611e6851b8c3f38b66b
Rejoy01 <rejoywilson65@gmail.com>
2023-11-30 22:06:29 +0530

    prisma

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser'
 
 import {userRoute} from './routes/userRoute.js'
 import { residencyRoute } from './routes/residencyRoute.js'
-
+// import
 import cors from 'cors'
 
 dotenv.config()
